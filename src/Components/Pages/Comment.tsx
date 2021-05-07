78d00d30d522b802d8f39f5a92e23f07d6117496
potatojoayo <potatojoayo@gmail.com>
2021-05-07 10:16:28 +0900

    utterances injection issue fixed

--- src/Components/Pages/Comment.tsx
+++ src/Components/Pages/Comment.tsx
@@ -1,19 +1,21 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 
 const Comment: React.FC = () => {
-	const Element = useRef(HTMLDivElement.prototype);
-	useEffect(() => {
-		const script = document.createElement("script");
-		const anchor = document.getElementById("inject-comments-for-uterances");
-		script.setAttribute("src", "https://utteranc.es/client.js");
-		script.setAttribute("crossorigin", "anonymous");
-		script.setAttribute("async", 'true');
-		script.setAttribute("repo", "potatojoayo/blog_comment");
-		script.setAttribute("issue-term", "url");
-		script.setAttribute("theme", "boxy-light");
-		anchor!.appendChild(script);
-	}, [Element])
-	return <div id="inject-comments-for-uterances" ref={Element}></div>
+	return <section ref={(element) => {
+		if (!element) {
+			return;
+		}
+		const scriptElem = document.createElement("script");
+		scriptElem.src = "https://utteranc.es/client.js";
+		scriptElem.async = true;
+		scriptElem.crossOrigin = "anonymous";
+		scriptElem.setAttribute("repo", "potatojoayo/blog_comment");
+		scriptElem.setAttribute("issue-term", "url");
+		scriptElem.setAttribute("label", "blog-comment");
+		scriptElem.setAttribute("theme", "boxy-light");
+		element.appendChild(scriptElem);
+	}}></section>
+
 }
 
 export default Comment;
